e1d5eb0d5f6f1496dcffcb1684105fdaef574da5
Alan Bruhns <alanbruhns@gmail.com>
2024-09-16 13:22:40 -0300

    Add GetAlan Query Use Case

---
+++ src/Application/Alans/AlanVm.cs
@@ -0,0 +1,7 @@
+namespace LDPIdentityAPI.Application.Alans;
+
+public class AlanVm
+{
+    public int Id { get; set; }
+}
+
---
+++ src/Web/Endpoints/Alans.cs
@@ -0,0 +1,19 @@
+﻿using LDPIdentityAPI.Application.Alans;
+using LDPIdentityAPI.Application.Alans.Queries.GetAlan;
+
+namespace LDPIdentityAPI.Web.Endpoints;
+
+public class Alans : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapGet(GetAlan, "{id}");
+    }
+
+    public Task<AlanVm> GetAlan(ISender sender, int id)
+        => sender.Send(new GetAlanQuery(id));
+    
+
+}
